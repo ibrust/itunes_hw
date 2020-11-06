e7f2c8f8ee11f978d8eac10280d7fc3319677ee0
ian rust <ibrust777@gmail.com>
2020-11-06 17:45:57 -0500

    created the cell class & wired it up mostly

---
+++ itunes_hw/Views/CustomCell.swift
@@ -0,0 +1,27 @@
+//
+//  CustomCell.swift
+//  itunes_hw
+//
+//  Created by Field Employee on 11/6/20.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    @IBOutlet weak var image_outlet: UIImageView!
+    
+    @IBOutlet weak var album_title_outlet: UILabel!
+    
+    @IBOutlet weak var artist_name_outlet: UILabel!
+    
+    
+    @IBAction func star_button(_ sender: UIButton) {
+    }
+    
+    
+}
