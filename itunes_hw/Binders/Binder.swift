dfbe688ad605b7aaa690e991d9c8384049d08b8b
ian rust <ibrust777@gmail.com>
2020-11-06 22:48:26 -0500

    developed the binder and controller a bit

--- itunes_hw/Binders/Binder.swift
+++ itunes_hw/Binders/Binder.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+
+
+class Binder {
+    
+    var request_manager: RequestManager
+    
+    
+    
+    // this is called by an observer of the model when the model updates
+    // it'll need to be passed an integer from the model somehow...
+    // the integer is the row that will be updated in the table
+    // the bound function will have access to the tableviews data - and thus the cell data through the integer, and also the model data through the intent functions (these are called within the closure)
+    // note that this accesses the cells data and sets them to the intent functions of the binder (which simply return the data). this is all after the binder has been alerted of changes via the observer (which calls this)
+    var bound_cellupdatehandler: ((Int) -> ())? = nil
+    
+    init(request_manager: RequestManager = RequestManager()){
+        self.request_manager = request_manager
+    }
+    
+    
+    func bind_cellupdatehandler(update_handler: @escaping (Int) -> ()){
+        self.bound_cellupdatehandler = update_handler
+    }
+    
+    
+}
+
+
+extension Binder{
+    func get_image(_ row: Int){
+        
+    }
+    func get_song_data(_ row: Int){
+        
+    }
+}
+
--- itunes_hw/Views/TableController.swift
+++ itunes_hw/Views/TableController.swift
@@ -10,73 +10,41 @@ import UIKit
 let reuse_id = "custom_cell"
 
 class TableController: UITableViewController {
+    
+    var binder = Binder()
+    
+    var current_image: UIImage? = nil
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+    
+        // in this bind i'll have to update the cells UI elements
+        // using the data fetched from the model.
+        // remember this is bound to an observer of the model.
+        // but when is the index passed in? where?
+        // does it come from the model? how?
+        // you could use the mutating cat method...
+        // and just use the song rank or whatever
+        // are these operations atomic? I have no idea...
+        // set this, within the request manager, on the main thread... synchronously
+        // could this specific operation be async though...?
+        // and the method in the request manager be sync? not sure...
+        // maybe try operations or some sort of synchronous queue in the request manager...? still not sure.
+        // or you could even come up with a mechanism that doesn't require this... like notifications maybe? not sure, really...
+        // some sort of array observation mechanism.......? duno. 
+        self.binder.bind_cellupdatehandler { [weak self] row in
+            DispatchQueue.main.async {
+                guard let self = self else{return}
+                
+                let index_path = IndexPath(row: row, section: 0)
+                let cell = self.tableView.cellForRow(at: index_path) as? CustomCell
+                let song_data = self.binder.get_song_data(row)
+                //cell?.album_title_outlet = song_data.albumTitle
+            }
+        }
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
@@ -89,3 +57,34 @@ class TableController: UITableViewController {
     */
 
 }
+
+// functions for view stating its intent
+// unlike in the example code, these aren't called by outlet actions but by cellforrowat and perhaps prefetch. these initiate the request loop.
+// the handler is the mechanism which updates the data... right?
+// yes, so you have the cell update handler which is actually called outside of cellforrowat, but it ultimately gets called as a consequence of the loop initiated in cellforrowat...
+extension TableController {
+    @objc func get_image(_ row: Int){
+        self.binder.get_image(row)
+    }
+    @objc func get_song_data(_ row: Int){
+        self.binder.get_song_data(row)
+    }
+}
+
+// tableview management functions
+extension TableController{
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+}
+
